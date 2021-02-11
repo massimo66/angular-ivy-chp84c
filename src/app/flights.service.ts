b2b6bb1fe4ed9d30e69d7b66f61c9f7652e8d035
massimo66 <marvelous966@hotmail.it>
2021-02-11 17:27:11 +0100

    1 file modified, 1 added

--- src/app/flights.service.ts
+++ src/app/flights.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Flight }  from './flight';
 
 @Injectable()
 export class FlightsService {
 
+   flights: Flight[] = [
+     {origin: "Miami", destination: 'Chicago', flightNumber: 345,
+     depart: '2020-02-02T23:18:21.932Z',
+     arrive: '2020-02-02T23:28:31.932Z', nonstop: true},
+     {origin: "Roma", destination: 'Napoli', flightNumber: 432,
+     depart: '2020-05-22T23:18:21.932Z',
+     arrive: '2020-05-22T23:18:21.932Z', nonstop: false}
+   ]
   constructor() { }
+  getFlights() { 
+   return this.flights;
+  }
+   postFlight(flight: Flight) {
+  }
 
+  deleteFlight(id: number) {
+    
+  }
 }

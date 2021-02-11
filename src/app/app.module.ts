88014e2eba9f999f1cc3ec585081b4eafc55670b
massimo66 <marvelous966@hotmail.it>
2021-02-11 12:29:18 +0100

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing.module';
 import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AdminComponent, HomeComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  declarations: [ AppComponent,  HomeComponent, AdminComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }

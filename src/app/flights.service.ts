7d5e2021366a85a1b8c6a22e4489c4383189b390
massimo66 <marvelous966@hotmail.it>
2021-02-12 09:59:51 +0100

    1 file modified

--- src/app/flights.service.ts
+++ src/app/flights.service.ts
@@ -5,12 +5,12 @@ import { Flight }  from './flight.model';
 export class FlightsService {
 
    flights: Flight[] = [
-     {origin: "Miami", destination: 'Chicago', flightNumber: 345,
-     depart: '2020-02-25T23:18:21.932Z',
-     arrive: '2020-02-25T23:28:25.932Z', nonstop: true},
-     {origin: "Roma", destination: 'Napoli', flightNumber: 432,
-     depart: '2020-05-22T21:18:21.932Z',
-     arrive: '2020-05-22T21:58:21.932Z', nonstop: false}
+     {origin: "Miami", destination: 'Chicago', flightNumber: 345,
+     depart: new Date('2020-02-25T23:18:21.932Z'),
+     arrive:  new Date('2020-02-25T23:21:21.932Z'), nonstop: true}, 
+     {origin: "New York", destination: 'Los Angeles', flightNumber: 432,
+     depart: new Date('2020-05-25T23:18:00.932Z'),  
+     arrive:  new Date('2020-05-25T23:23:21.932Z'), nonstop: false},
    ]
   constructor() { }
   getFlights() { 

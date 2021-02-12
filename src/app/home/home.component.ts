53327c0e572c430db3d1e2045214cd4792c29b2c
massimo66 <marvelous966@hotmail.it>
2021-02-12 10:36:53 +0100

    2 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FlightsService } from '../flights.service';
+import { Flight }  from '../flight.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  flights: Flight[];
   constructor(private flightsService: FlightsService) { }
 
-  ngOnInit() {
+  ngOnInit(): void  {
+    this.flights = this.flightsService.getFlights();
   }
 
   getFlights() {

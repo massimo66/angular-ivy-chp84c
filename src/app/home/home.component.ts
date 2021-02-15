14a4eed14f6ed27c4f9e013d7ee77ff7e1bfd922
massimo66 <marvelous966@hotmail.it>
2021-02-15 09:08:50 +0100

    2 files modified, 2 added, 1 deleted

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { Flight }  from '../flight.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   flights: Flight[];
